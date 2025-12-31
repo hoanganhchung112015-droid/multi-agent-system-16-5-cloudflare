25bace19f56db272ffa921dfd430fa3e0685c55c
hoanganhchung112015-droid <hoanganhchung112015@gmail.com>
2025-12-31 12:59:35 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        // Ép Vite tạo tên file mới mỗi lần build để tránh cache Cloudflare
-        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
-      },
-    },
-  },
-});
+    emptyOutDir: true,
+  }
+})
